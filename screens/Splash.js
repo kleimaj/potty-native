63eb642ee03c43d39d58134ef2b15b821fbd46f0
Jacob Kleiman <33333976+kleimaj@users.noreply.github.com>
2020-11-06 23:43:33 -0800

    removes additional styled component in splash

--- screens/Splash.js
+++ screens/Splash.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { View } from 'react-native';
 import { Display } from '../utils';
-import styled from '@emotion/native';
+import styled, { css } from '@emotion/native';
 
 const Container = styled.View`
   flex: 1;
   align-items: center;
-`;
-const Title = styled(Display)`
-  align-self: flex-start;
   margin: 25px;
   margin-top: 100px;
-  font-family: Avenir Next;
-  font-size: 42px;
 `;
 export const Splash = () => {
   return (
     <Container>
-      <Title>Potty Angel</Title>
+      <Display
+        style={css`
+          align-self: flex-start;
+        `}
+      >
+        Potty Angel
+      </Display>
     </Container>
   );
 };
