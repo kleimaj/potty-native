6746bf1826a4338562ff983398c0d599e0d3f57e
Jacob Kleiman <33333976+kleimaj@users.noreply.github.com>
2020-11-18 22:30:30 -0800

    adds React to context

--- hooks/context.js
+++ hooks/context.js
@@ -1,17 +1,17 @@
-import { useState, createContext } from 'react';
+import React, { useState, createContext } from 'react';
 import UserModel from '../models';
 
 const UserContext = createContext([], () => {});
 
-const UserContextProvider = props => {
-    const [user, setUser] = useState({
+const UserContextProvider = (props) => {
+  const [user, setUser] = useState({
     // get from async storage
-    });
-    return (
-        <UserContext.Provider value={[user, setUser]}>
-            {props.children}
-        </UserContext.Provider>
-    )
-}
+  });
+  return (
+    <UserContext.Provider value={[user, setUser]}>
+      {props.children}
+    </UserContext.Provider>
+  );
+};
 
 export { UserContext, UserContextProvider };
