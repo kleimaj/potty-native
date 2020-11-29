dc1c3cfda05ec5b37f38d3c0bff2b7e50f675514
Jacob Kleiman <33333976+kleimaj@users.noreply.github.com>
2020-11-28 17:35:32 -0800

    implements useComments hook

---
+++ hooks/useComments.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+import Constants from 'expo-constants';
+
+const { manifest } = Constants;
+
+// const REACT_APP_API_URL = 'http://localhost:3000';
+const REACT_APP_API_URL = `http://${manifest.debuggerHost
+  .split(':')
+  .shift()}:3000`;
+
+export const useComments = ({ id }) => {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    async function getComments(pottyId) {
+      const res = await fetch(`${REACT_APP_API_URL}/comment/${pottyId}`);
+      const returnedComments = await res.json();
+      console.log(returnedComments);
+      setComments(returnedComments);
+    }
+    getComments(id);
+  }, [id]);
+
+  return comments;
+};
