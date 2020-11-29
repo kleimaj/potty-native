d565ece617648a979bfcba668a741bf6aaacf77a
Jacob Kleiman <33333976+kleimaj@users.noreply.github.com>
2020-11-29 15:15:18 -0800

    Adds author to comments

--- components/Comment.js
+++ components/Comment.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import styled from '@emotion/native';
+import { css } from '@emotion/native';
 import { Rating } from './Rating';
 import { Small, Smallest } from '../utils';
 
+const Container = styled.View`
+  padding: 12px;
+  margin: 0 12px;
+`;
 const Card = styled.View`
   border-radius: 16px;
   background: white;
-  padding: 25px;
-  margin: 18px;
+  padding: 18px;
 `;
 
 export const Comment = (props) => {
   console.log(props.comment.item, '!');
-  const { title, rating, body } = props.comment.item;
+  const { title, rating, body, author } = props.comment.item;
   console.log(title, rating, body);
   console.log('~~~~~~~~');
   return (
-    <Card>
-      <Small>{title}</Small>
-      <Rating rating={parseInt(rating, 10)} />
-      <Smallest>{body}</Smallest>
-    </Card>
+    <Container>
+      <Card>
+        <Small>{title}</Small>
+        <Rating rating={parseInt(rating, 10)} />
+        <Smallest>{body}</Smallest>
+      </Card>
+      <Smallest
+        style={css`
+          align-self: flex-end;
+        `}
+      >
+        By {author} 👼
+      </Smallest>
+    </Container>
   );
 };
