670cf164030e59de3949648fdfdcdeb67c7516aa
Jacob Kleiman <33333976+kleimaj@users.noreply.github.com>
2020-11-29 18:05:24 -0800

    adds primary button, scrollview to potty screen

--- screens/Potty.js
+++ screens/Potty.js
@@ -1,13 +1,28 @@
 import React, { useState } from 'react';
 import { Container, Header, Subhead, Body, Small, Smallest } from '../utils';
 import { useComments } from '../hooks';
-import { Rating, EditableRating, Comment, Input } from '../components';
+import {
+  Rating,
+  PrimaryButton,
+  EditableRating,
+  Comment,
+  Input,
+} from '../components';
 import styled, { css } from '@emotion/native';
 import { FlatList } from 'react-native-gesture-handler';
 
 const CommentContainer = styled.View`
   height: 300px;
 `;
+const Main = styled.View`
+  width: 100%;
+  height: 100px;
+`;
+const Inputs = styled.View`
+  padding: 18px;
+  align-items: center;
+`;
+const Scroll = styled.ScrollView``;
 
 export const Potty = ({ route, navigation }) => {
   /*"address": "225 Bush Street, San Francisco, CA 94104",
@@ -47,12 +62,22 @@ export const Potty = ({ route, navigation }) => {
           ListEmptyComponent={<Body>No comments yet!</Body>}
         />
       </CommentContainer>
-      {/* <Subhead>Add a comment</Subhead>
-      <Smallest>Title</Smallest>
-      <Input onChangeText={setTitle} value={title} />
-      <Smallest>Body</Smallest>
-      <Input onChangeText={setBody} value={body} /> */}
-      <EditableRating currRating={currRating} setCurrRating={setCurrRating} />
+      <Main>
+        <Scroll>
+          <Small>Add a comment</Small>
+          <Inputs>
+            <Smallest>Title</Smallest>
+            <Input onChangeText={setTitle} value={title} />
+            <EditableRating
+              currRating={currRating}
+              setCurrRating={setCurrRating}
+            />
+            <Smallest>Body</Smallest>
+            <Input onChangeText={setBody} value={body} />
+            <PrimaryButton onPress={() => {}}>Post</PrimaryButton>
+          </Inputs>
+        </Scroll>
+      </Main>
     </Container>
   ) : (
     <Container>
