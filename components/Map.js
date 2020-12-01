83cf6108b20c9eb5b15565259a6291315b6cedd2
Jacob Kleiman <33333976+kleimaj@users.noreply.github.com>
2020-11-30 18:08:49 -0800

    :sparkles: adds emergency button feature

--- components/Map.js
+++ components/Map.js
@@ -1,9 +1,9 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { InfoWindow } from './InfoWindow';
-import MapView, { Marker, Callout } from 'react-native-maps';
+import MapView, { Callout } from 'react-native-maps';
 import { usePotties } from '../hooks';
 import { useNavigation } from '@react-navigation/native';
-import { TargetButton } from './Buttons';
+import { TargetButton, PrimaryButton } from './Buttons';
 // import styled from '@emotion/native';
 
 const mapStyles = {
@@ -12,14 +12,39 @@ const mapStyles = {
 };
 export const Map = ({ location, map, ready, setReady }) => {
   const navigation = useNavigation();
-
+  const [markers, setMarkers] = useState([]);
   const [currRegion, setRegion] = useState();
   const potties = usePotties(
     location.coords.latitude,
     location.coords.longitude,
   );
-  console.log(potties);
-  console.log(typeof potties);
+
+  useEffect(() => {
+    if (potties.length) {
+      setMarkers(
+        potties.map((marker, idx) => (
+          <MapView.Marker
+            key={idx}
+            coordinate={{
+              latitude: parseFloat(marker.latitude),
+              longitude: parseFloat(marker.longitude),
+            }}
+            // title={marker.name}
+            // description={`Rating: ${marker.rating}`}
+            onCalloutPress={() => navigation.navigate('Potty', marker)}
+          >
+            <Callout tooltip>
+              <InfoWindow
+                name={marker.name}
+                address={marker.address}
+                rating={marker.rating}
+              />
+            </Callout>
+          </MapView.Marker>
+        )),
+      );
+    }
+  }, [potties, navigation]);
 
   const recenter = () => {
     console.log(map);
@@ -45,29 +70,47 @@ export const Map = ({ location, map, ready, setReady }) => {
           longitudeDelta: 0.0121 * 5,
         }}
       >
-        {potties &&
-          potties.map((marker, idx) => (
-            <Marker
-              key={idx}
-              coordinate={{
-                latitude: parseFloat(marker.latitude),
-                longitude: parseFloat(marker.longitude),
-              }}
-              // title={marker.name}
-              // description={`Rating: ${marker.rating}`}
-              onCalloutPress={() => navigation.navigate('Potty', marker)}
-            >
-              <Callout tooltip>
-                <InfoWindow
-                  name={marker.name}
-                  address={marker.address}
-                  rating={marker.rating}
-                />
-              </Callout>
-            </Marker>
-          ))}
+        {
+          markers
+          // potties.map((marker, idx) => (
+          //   <Marker
+          //     key={idx}
+          //     coordinate={{
+          //       latitude: parseFloat(marker.latitude),
+          //       longitude: parseFloat(marker.longitude),
+          //     }}
+          //     // title={marker.name}
+          //     // description={`Rating: ${marker.rating}`}
+          //     onCalloutPress={() => navigation.navigate('Potty', marker)}
+          //   >
+          //     <Callout tooltip>
+          //       <InfoWindow
+          //         name={marker.name}
+          //         address={marker.address}
+          //         rating={marker.rating}
+          //       />
+          //     </Callout>
+          //   </Marker>
+          // ))
+        }
       </MapView>
       <TargetButton onPress={recenter} />
+      <PrimaryButton
+        onPress={() => {
+          if (potties.length) {
+            map.current.animateToRegion({
+              latitude: potties[0].latitude,
+              longitude: potties[0].longitude,
+              latitudeDelta: 0.015 * 0.5,
+              longitudeDelta: 0.0121 * 0.5,
+            });
+          } else {
+            console.log('no potties in the area');
+          }
+        }}
+      >
+        Emergency
+      </PrimaryButton>
     </>
   );
 };
