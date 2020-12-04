cff12d74a21e66a9ccf49d34e4e78cc5aba42e5b
Jacob Kleiman <33333976+kleimaj@users.noreply.github.com>
2020-12-04 10:36:40 -0800

    adds button container to modal

--- components/Modal.js
+++ components/Modal.js
@@ -3,12 +3,19 @@ import { Subhead } from '../utils';
 import styled from '@emotion/native';
 import { Input } from './Input';
 import { EditableRating } from './Rating';
+import { PrimaryButton, SecondaryButton } from './Buttons';
 
 const ModalWrapper = styled.View`
   background: #fff;
   width: 300px;
   padding: 12px 24px;
   height: 300px;
+  align-items: center;
+`;
+const ButtonContainer = styled.View`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 export const AddPottyModal = () => {
@@ -18,6 +25,10 @@ export const AddPottyModal = () => {
       <Input />
       <Subhead>Rating</Subhead>
       <EditableRating />
+      <ButtonContainer>
+        <PrimaryButton>Create Potty</PrimaryButton>
+        <SecondaryButton>Cancel</SecondaryButton>
+      </ButtonContainer>
     </ModalWrapper>
   );
 };
